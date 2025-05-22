ba9a47a1eb1598f798c202f5424ae967604af654
Jaemin Park <jemin7709@gmail.com>
2025-05-23 01:07:54 +0900

    fix: 저장할 논문 파일 이름 변경

--- scripts/process_related_papers.sh
+++ scripts/process_related_papers.sh
@@ -1,7 +1,7 @@
-uv run ./src/process_related_papers.py --file ./data/cvpr2024.csv
-uv run ./src/process_related_papers.py --file ./data/iccv2024.csv
-uv run ./src/process_related_papers.py --file ./data/eccv2024.csv
-uv run ./src/process_related_papers.py --file ./data/aaai2024.csv
-uv run ./src/process_related_papers.py --file ./data/iclr2024.csv
-uv run ./src/process_related_papers.py --file ./data/icml2024.csv
-uv run ./src/process_related_papers.py --file ./data/neurips2024.csv
+uv run ./src/process_related_papers.py --file ./data/cvpr_24.csv
+uv run ./src/process_related_papers.py --file ./data/iccv_24.csv
+uv run ./src/process_related_papers.py --file ./data/eccv_24.csv
+uv run ./src/process_related_papers.py --file ./data/aaai_24.csv
+uv run ./src/process_related_papers.py --file ./data/iclr_24.csv
+uv run ./src/process_related_papers.py --file ./data/icml_24.csv
+uv run ./src/process_related_papers.py --file ./data/neurips_24.csv
